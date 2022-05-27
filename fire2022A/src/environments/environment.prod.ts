91c26429bf242c41dcb0e8246cd2779892236efb
luckLuis <jacomeluis7@gmail.com>
2022-05-26 22:24:46 -0500

    25052022

---
+++ fire2022A/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'proyectos-2022a-cdc6c',
+    appId: '1:347734439295:web:414eb3d4d3f99a874b964a',
+    storageBucket: 'proyectos-2022a-cdc6c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA2F9QEVG7BOzeGQRgOBecDt3WObM0YVtI',
+    authDomain: 'proyectos-2022a-cdc6c.firebaseapp.com',
+    messagingSenderId: '347734439295',
+    measurementId: 'G-7351K56B25',
+  },
+  production: true
+};
---
+++ fire2022A/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'proyectos-2022a-cdc6c',
+    appId: '1:347734439295:web:414eb3d4d3f99a874b964a',
+    storageBucket: 'proyectos-2022a-cdc6c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA2F9QEVG7BOzeGQRgOBecDt3WObM0YVtI',
+    authDomain: 'proyectos-2022a-cdc6c.firebaseapp.com',
+    messagingSenderId: '347734439295',
+    measurementId: 'G-7351K56B25',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
